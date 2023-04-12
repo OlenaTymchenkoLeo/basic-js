a52cf2a73a21ea112061548ab6364a7cfc885304
OlenaTymchenkoLeo <124084136+OlenaTymchenkoLeo@users.noreply.github.com>
2023-04-13 00:10:03 +0300

    feat: add solutions 5 tasks

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,23 +1,45 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Create name of dream team based on the names of its members
- *  
- * @param {Array} members names of the members 
+ *
+ * @param {Array} members names of the members
  * @return {String | Boolean} name of the team or false
  * in case of incorrect members
  *
  * @example
- * 
+ *
  * createDreamTeam(['Matt', 'Ann', 'Dmitry', 'Max']) => 'ADMM'
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(arrFriends) {
+  if (!arrFriends) {
+    return false;
+  }
+  if (typeof arrFriends !== "object") {
+    return false;
+  }
+  let abr = [];
+  let arrFriends1 = [];
+  console.log(arrFriends.length, arrFriends[0]);
+  if (arrFriends.length === 1 && typeof arrFriends[0] === "object") {
+    arrFriends1 = arrFriends.flat(1);
+  } else arrFriends1 = arrFriends;
+  console.log(arrFriends);
+  console.log(arrFriends1);
+  for (let i = 0; i < arrFriends1.length; i++) {
+    let currentName = arrFriends1[i];
+
+    if (typeof currentName === "string") {
+      let nameTrim = currentName.trim();
+      abr.push(nameTrim[0].toUpperCase());
+    }
+  }
+  let abrUpper = abr.sort().join("");
+  return abrUpper;
 }
 
 module.exports = {
-  createDreamTeam
+  createDreamTeam,
 };
--- src/what-season.js
+++ src/what-season.js
@@ -1,21 +1,73 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Extract season from given date and expose the enemy scout!
- * 
+ *
  * @param {Date | FakeDate} date real or fake date
  * @returns {String} time of the year
- * 
+ *
  * @example
- * 
+ *
  * getSeason(new Date(2020, 02, 31)) => 'spring'
- * 
+ *
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getSeason(date) {
+  if (!date) {
+    return "Unable to determine the time of year!";
+  }
+  console.log(Object.getOwnPropertyNames(date).length);
+  if (Object.getOwnPropertyNames(date).length > 0 || !date.getTime)
+    throw new Error("Invalid date!");
+
+  let answer = "";
+  let month = date.getMonth();
+  console.log(month);
+
+  switch (month) {
+    case 11:
+      answer = "winter";
+      break;
+    case 0:
+      answer = "winter";
+      break;
+    case 1:
+      answer = "winter";
+      break;
+    case 2:
+      answer = "spring";
+      break;
+    case 3:
+      answer = "spring";
+      break;
+    case 4:
+      answer = "spring";
+      break;
+    case 5:
+      answer = "summer";
+      break;
+    case 6:
+      answer = "summer";
+      break;
+    case 7:
+      answer = "summer";
+      break;
+    case 8:
+      answer = "autumn";
+      break;
+    case 9:
+      answer = "autumn";
+      break;
+    case 10:
+      answer = "autumn";
+      break;
+    default:
+      answer = "Unable to determine the time of year!";
+      break;
+  }
+
+  return answer;
 }
 
 module.exports = {
-  getSeason
+  getSeason,
 };
